019e472221478c281dec541a42c5a63fa3690552
Tomer Iwan <t.iwan@vu.nl>
2024-03-04 08:45:36 +0100

    feat: update data source now

--- R/update_data_source_now.R
+++ R/update_data_source_now.R
@@ -1,16 +1,16 @@
-#' Run Extract Refresh Task
+#' Update Data Source Now
 #'
-#' Runs the specified extract refresh task on Tableau Server.
+#' Runs an extract refresh on the specified data source.
 #'
 #' @param tableau A list containing the Tableau authentication variables: `base_url`, `token`, and `site_id`.
-#' @param task_id The ID of the extract refresh task to run.
+#' @param datasource_id The ID of the data source to refresh.
 #' @param api_version The API version to use (default: 3.22).
 #'
 #' @return The response from the API.
 #' @export
 #'
 #' @family Tableau REST API
-run_extract_refresh_task <- function(tableau, task_id, api_version = 3.22) {
+update_data_source_now <- function(tableau, datasource_id, api_version = 3.22) {
   base_url <- tableau$base_url
   token <- tableau$token
   site_id <- tableau$site_id
@@ -21,13 +21,13 @@ run_extract_refresh_task <- function(tableau, task_id, api_version = 3.22) {
     api_version,
     "/sites/",
     site_id,
-    "/tasks/extractRefreshes/",
-    task_id,
-    "/runNow"
+    "/datasources/",
+    datasource_id,
+    "/refresh"
   )
 
   # Construct the request body
-  # As per the requirements, the request body can be empty for now
+  # The request body is empty as per the requirements
   request_body <- "<tsRequest></tsRequest>"
 
   # Make the POST request to the Tableau REST API
